686c71fce5e8df20abfc7179db7aed0c54c8df12
David Snow <dsnow@techaweb.com>
2026-08-10 14:18:32 -0700

    Fixes placement of header comment.

--- ds-snippets/snippets/disabled-admin-bar.php
+++ ds-snippets/snippets/disabled-admin-bar.php
@@ -1,11 +1,13 @@
-/**
-* Plugin Name: Disable Admin Bar for Non-Admins
-* Description: Disables the admin bar for non-admin users.
-* Version: 1.0
-* Author: David Snow
-* Author URI: https://davidsnow.net
-*/
 <?php
+
+/**
+ * Plugin Name: Disable Admin Bar for Non-Admins
+ * Description: Disables the admin bar for non-admin users.
+ * Version: 1.0
+ * Author: David Snow
+ * Author URI: https://davidsnow.net
+ */
+
 add_action('wp', function () {
   if (! current_user_can('manage_options')) {
     show_admin_bar(false);
